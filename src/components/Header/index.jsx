8dcb2375f26a32dd9be8c1cdb12f6aa14ee30b35
liuzixu <liuzixu@xiaomi.com>
2018-11-01 19:40:39 +0800

    delete import

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.scss'
-import './index1.scss'
+// import './index1.scss'
 
 export default class Header extends React.Component {
     constructor (props){
