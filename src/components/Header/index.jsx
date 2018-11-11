695b925fe1d64c906ac99fbb2ec7aba5fd61d3e5
liuzixu <liuzixu@xiaomi.com>
2018-11-11 17:42:33 +0800

    master_1 1

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,7 +11,8 @@ export default class Header extends React.Component {
             <>
                 <h1>Hello World! from {this.props.author}</h1>
                 <h2>master here</h2>
-                <h2>test here master branch</h2>
+                <h2>test here</h2>
+                <h3>master_1 1</h3>
                 <hr/>
             </>
         )
